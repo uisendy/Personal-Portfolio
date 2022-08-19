b6a136fb3b249013de62ae04f249313dcb2df889
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-19 18:28:18 +0100

    [f-design]: update: Nav-bar design and CSS

--- src/components/Navbar/Menu.js
+++ src/components/Navbar/Menu.js
@@ -1,89 +1,76 @@
 import React from "react";
 import { useState } from "react";
+import "./style.css";
+import { BsArrowRight } from "react-icons/bs";
 
 const Menu = () => {
-  const [isNavOpen, setIsNavOpen] = useState(false); // initiate isNavOpen state with false
+  const [isNavOpen, setIsNavOpen] = useState(false);
 
   return (
-    <div className="flex items-center justify-between border-b border-gray-400 py-8">
-      <a href="/">
-        <img src="https://designbygio.it/images/logo.png" alt="logo" />
-      </a>
-      <nav>
-        <section className="MOBILE-MENU flex lg:hidden">
+    <div className="py-8 px-8 bg-black">
+      <nav className="flex justify-between">
+        <section className="flex">
           <div
-            className="HAMBURGER-ICON space-y-2"
-            onClick={() => setIsNavOpen((prev) => !prev)} // toggle isNavOpen state on click
+            onClick={() => setIsNavOpen((open) => !open)}
+            className={
+              isNavOpen
+                ? "hamburger-menu is-active"
+                : "hamburger-menu space-y-2"
+            }
           >
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
-            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            {/* <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span> */}
+            <div className="menu-icon"></div>
           </div>
 
           <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
             {" "}
             <div
-              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
               onClick={() => setIsNavOpen(false)} // change isNavOpen state to false to close the menu
-            >
-              <svg
-                className="h-8 w-8 text-gray-600"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-              >
-                <line x1="18" y1="6" x2="6" y2="18" />
-                <line x1="6" y1="6" x2="18" y2="18" />
-              </svg>
-            </div>
-            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/about">About</a>
+            ></div>
+            <ul className="flex flex-col gap-20 justify-between min-h-[250px] w-[80%] text-white text-9xl">
+              <li className="flex">
+                <a
+                  className="links flex item-centre hover:italic"
+                  href="/about"
+                >
+                  About <BsArrowRight className="arrow" />
+                </a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/portfolio">Portfolio</a>
+              <li className="flex">
+                <a
+                  className="links flex item-centre hover:italic"
+                  href="/portfolio"
+                >
+                  Portfolio <BsArrowRight className="arrow" />
+                </a>
               </li>
-              <li className="border-b border-gray-400 my-8 uppercase">
-                <a href="/contact">Contact</a>
+              <li className="flex">
+                <a
+                  className="links flex item-centre hover:italic"
+                  href="/contact"
+                >
+                  Contact <BsArrowRight className="arrow" />
+                </a>
               </li>
             </ul>
           </div>
         </section>
-
-        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
-          <li>
-            <a href="/about">About</a>
-          </li>
-          <li>
-            <a href="/portfolio">Portfolio</a>
-          </li>
-          <li>
-            <a href="/contact">Contact</a>
-          </li>
-        </ul>
+        <div className="">
+          <a
+            className="z-[40] relative text-white text-2xl md:text-4xl"
+            href="/contact"
+          >
+            S<span className="italic">i:</span>{" "}
+          </a>
+        </div>
+        <div className="contact hidden items-center gap-2 z-[40]  pr-2 text-white font-playFairSc text-2xl md:flex md:text-4xl md:pr-10 md:relative">
+          <a className="" href="/contact">
+            CONTACT
+          </a>
+          <BsArrowRight className="arrow" />
+        </div>
       </nav>
-      <style>{`
-    .hideMenuNav {
-      display: none;
-    }
-    .showMenuNav {
-      display: block;
-      position: absolute;
-      width: 100%;
-      height: 100vh;
-      top: 0;
-      left: 0;
-      background: white;
-      z-index: 10;
-      display: flex;
-      flex-direction: column;
-      justify-content: space-evenly;
-      align-items: center;
-    }
-  `}</style>
     </div>
   );
 };
