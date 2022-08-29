60dce8f58e40e68f76ef61877d8a6902ca530f32
Inieke Sendy <ini.e.sendy@gmail.com>
2022-08-29 07:24:41 +0100

    [f-design}: Update: Change Header component to show from left

--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -3,8 +3,10 @@ import React from "react";
 const SectionHeader = ({ title, subtitle }) => {
   return (
     <div className="text-white py-16 px-[6.5%]">
-      <h1 className="text-4xl md:text-6xl">{title}</h1>
-      <p className="italic text-2xl md:text-4xl pt-7">{subtitle}</p>
+      <h1 className="text-4xl md:text-6xl text-right pr-24">{title}</h1>
+      <p className=" font-montserrat text-sm pt-7 text-right pr-24">
+        {subtitle}
+      </p>
     </div>
   );
 };
