d9a3b8b6c437fdaa32b0741099c856d0cd8d3661
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-06 05:45:44 +0100

    [f-design]: Update data-scroll for LocoScroll

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,10 @@ import SectionHeader from "../SectionHeader/SectionHeader";
 
 const About = () => {
   return (
-    <section className="bg-black text-white w-[100vW] px-[6.5%]">
+    <section
+      className="bg-black text-white w-[100vW] px-[6.5%]"
+      data-scroll-section
+    >
       <SectionHeader title="About Me" />
       <div className="w-[100%]">
         <p className="">
