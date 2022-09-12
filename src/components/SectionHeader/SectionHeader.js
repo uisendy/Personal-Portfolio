dbf0ed45a21011b7e6aee2fbac91e1f09014db96
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-13 00:23:06 +0100

    [f-design]: Update: Responsive feature added for mobile

--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const SectionHeader = ({ title, subtitle, ident }) => {
+const SectionHeader = ({ title, subtitle }) => {
   return (
-    <div ref={ident} className="text-white py-16 px-[6.5%]">
-      <h1 className="text-4xl md:text-6xl text-right pr-24">{title}</h1>
-      <p className=" font-montserrat text-sm pt-7 text-right pr-24">
+    <div className=" text-white py-16 px-10 md:px-[6.5%]">
+      <h1 className=" font-playFairSc text-4xl md:text-6xl text-center md:text-right md:pr-24">
+        {title}
+      </h1>
+      <p className=" font-montserrat text-sm pt-7 text-center md:text-right md:pr-24">
         {subtitle}
       </p>
     </div>
