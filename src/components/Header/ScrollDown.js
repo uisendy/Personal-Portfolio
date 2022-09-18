ba989c3489e9a30571746d4ad08341bcd31839e1
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-18 20:06:29 +0100

    [f-design]: Add: Animated Scrolldown icon

---
+++ src/components/Header/ScrollDown.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './scrolldown.css';
+
+const ScrollDown = () => {
+  return (
+    <div class="container">
+      <div class="chevron"></div>
+      <div class="chevron"></div>
+      <div class="chevron"></div>
+      <span class="text font-playFairSc text-sm font-thin">Scroll down</span>
+    </div>
+  );
+};
+
+export default ScrollDown;
