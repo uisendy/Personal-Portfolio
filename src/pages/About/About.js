a23a453e051ee8f17022d9effa6d6f43d8f45bab
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 01:50:35 +0100

    [f-about]: Add: AboutPage component

---
+++ src/pages/About/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Hero from './Hero';
+
+const About = () => {
+  return (
+    <section className="bg-black text-white px-[6.5%]" data-scroll-section>
+      <Hero />
+    </section>
+  );
+};
+
+export default About;
