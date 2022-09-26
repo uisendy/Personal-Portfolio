8397e4eaeee5df9814e8bed4341c1f85abbdcd87
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 01:46:41 +0100

    [f-about]: update: update setIsNavOpen props

--- src/components/Navbar/MenuList.js
+++ src/components/Navbar/MenuList.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const MenuList = ({ menu, BsArrowUpRight }) => {
+const MenuList = ({ menu, BsArrowUpRight, setIsNavOpen }) => {
   return (
     <>
       <li className="flex">
         <Link
           className="links font-playFairSc uppercase text-5xl md:text-7xl flex item-centre hover:italic"
           to={menu.link}
+          onClick={() => setIsNavOpen((open) => !open)}
         >
           {menu.name} <BsArrowUpRight className="arrow" />
         </Link>
