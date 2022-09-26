adaeacb724e5ab678f281143537655da1cc071f1
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 23:18:43 +0100

    [f-about]: Add: Home Section

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../../components/Header/Header';
+import Featured from '../../components/Featured/Featured';
+import Footer from '../../components/Footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Featured />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
