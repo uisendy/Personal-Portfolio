f673dd741773271ff0612d287ec2fee8bff05afa
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 23:18:27 +0100

    [f-about]: Add: Skills Section

---
+++ src/pages/About/Skills.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import SectionHeader from '../../components/SectionHeader/SectionHeader';
+
+const Skills = () => {
+  return (
+    <div data-scroll-section className=" mb-32">
+      <SectionHeader
+        title={'My Skills'}
+        subtitle={'& Professional Experience'}
+      />
+    </div>
+  );
+};
+
+export default Skills;
