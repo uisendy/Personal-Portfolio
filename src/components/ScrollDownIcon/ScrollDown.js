31bc7aa2d8a9729247a07041ccc364165d2e9878
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 23:13:27 +0100

    [f-about]: Update:Props

--- src/components/ScrollDownIcon/ScrollDown.js
+++ src/components/ScrollDownIcon/ScrollDown.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './scrolldown.css';
 
-const ScrollDown = ({ xPosition, yPosition }) => {
+const ScrollDown = ({ xPosition, yPosition, position }) => {
   return (
     <div
       className="container"
       style={{
-        position: 'fixed',
+        position: position,
         width: '24px',
         height: '24px',
         top: yPosition,
