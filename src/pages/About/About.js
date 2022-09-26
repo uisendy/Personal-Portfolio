96daa532ca0215ca30fff9e939a5bca1c11f8653
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 23:17:22 +0100

    [f-about]: Add: Description and Skills Componet

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Hero from './Hero';
+import Description from './Description';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
+import ScrollToTop from '../../components/ScrollDownIcon/ScrollToTop';
+import Skills from './Skills';
 
 const About = () => {
+  gsap.registerPlugin(ScrollTrigger);
+  useEffect(() => {
+    <ScrollToTop />;
+    ScrollTrigger.refresh();
+  }, []);
   return (
-    <section className="bg-black text-white px-[6.5%]" data-scroll-section>
+    <section className="bg-black text-white px-[6.5%]">
       <Hero />
+      <Description />
+      <Skills />
     </section>
   );
 };
