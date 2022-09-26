216dab63d455e62403db6ea36d0a3192cf4eae17
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-26 23:14:45 +0100

    [f-about]: update: UI friendly SectionHeader

--- src/components/SectionHeader/SectionHeader.js
+++ src/components/SectionHeader/SectionHeader.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { MdOutlineCode } from 'react-icons/md';
 
 const SectionHeader = ({ title, subtitle }) => {
   return (
-    <div className=" text-white py-16 px-10 md:px-[6.5%]">
-      <h1 className=" font-playFairSc text-4xl md:text-6xl text-center md:text-right md:pr-24">
-        {title}
-      </h1>
-      <p className=" font-montserrat text-sm pt-7 text-center md:text-right md:pr-24">
-        {subtitle}
-      </p>
+    <div className=" text-white pt-16 pb-10 px-10 md:px-[6.5%] md:pr-24 flex justify-end items-center gap-10 opacity-50 hover:opacity-100 transition-all ease-in-out delay-150 duration-300">
+      <div>
+        <h2 className=" font-playFairSc text-4xl md:text-6xl text-left md:text-right uppercase">
+          {title}
+        </h2>
+        <h3 className=" text-3xl md:text-5xl text-left md:text-right italic ">
+          {subtitle}
+        </h3>
+      </div>
     </div>
   );
 };
