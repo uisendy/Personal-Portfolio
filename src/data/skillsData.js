2049a2fa6d1f21ba44c76083a1edf7b424547e0d
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-29 21:43:32 +0100

    [f-about]: Update : add: Skills data object

---
+++ src/data/skillsData.js
@@ -0,0 +1,68 @@
+import html5 from '../Assets/Logo/html5.svg';
+import css3 from '../Assets/Logo/css3-alt.svg';
+import react from '../Assets/Logo/react.svg';
+import js from '../Assets/Logo/js.svg';
+
+const skillsSet = {
+  languages: [
+    { id: 1, item: html5, progress: '85' },
+    { id: 2, item: css3, progress: '75' },
+    { id: 3, item: js, progress: '79' },
+    { id: 4, item: react, progress: '60' },
+  ],
+  myTools: [
+    {
+      id: 1,
+      heading: 'Development platform',
+      items: [
+        { id: 1, item: 'VScode' },
+        { id: 2, item: 'Atom' },
+        { id: 3, item: 'Figma' },
+      ],
+    },
+    {
+      id: 2,
+      heading: 'Organization Tools',
+      items: [
+        { id: 1, item: 'GitHub' },
+        { id: 2, item: 'Slack' },
+        { id: 3, item: 'Discord' },
+        { id: 4, item: 'Zendesk' },
+        { id: 5, item: 'Zoom, Teams, Meet, etc' },
+      ],
+    },
+  ],
+  myProductions: [
+    {
+      id: 1,
+      heading: 'Front-end',
+      items: [
+        { id: 1, item: 'E-commerce' },
+        { id: 2, item: 'Landing page' },
+        { id: 3, item: 'Showcase website' },
+        { id: 4, item: 'Reusable Components' },
+      ],
+    },
+
+    {
+      id: 2,
+      heading: 'Back-end ',
+      items: [
+        { id: 1, item: 'Software Project Management' },
+        { id: 2, item: 'Reusable Components' },
+        { id: 3, item: 'Open Source Development' },
+        { id: 4, item: 'Pair-Programming' },
+      ],
+    },
+    {
+      id: 3,
+      heading: 'Mobile Application Development',
+      items: [
+        { id: 1, item: 'Native Apps' },
+        { id: 2, item: 'Application & WebApp' },
+      ],
+    },
+  ],
+};
+
+export default skillsSet;
