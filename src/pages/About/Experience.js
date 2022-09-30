a388798dedd4d139a148111d06611977cba56874
Inieke Sendy <ini.e.sendy@gmail.com>
2022-09-30 11:16:55 +0100

    [f-about]: update: Add Header component

--- src/pages/About/Experience.js
+++ src/pages/About/Experience.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import SectionHeader from '../../components/SectionHeader/SectionHeader';
 
 const Experience = () => {
-  return <div>Experience</div>;
+  return (
+    <div data-scroll-section className=" ">
+      <SectionHeader title="Experiences" subtitle={'& Trainings'} />
+    </div>
+  );
 };
 
 export default Experience;
