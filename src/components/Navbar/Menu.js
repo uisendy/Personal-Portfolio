75ebda62fcb47381ef2cab3ec48dd268e9bf1ee4
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-03 18:56:52 +0100

    [f-about]: update: code cleaning and UI optimization. State Handling

--- src/components/Navbar/Menu.js
+++ src/components/Navbar/Menu.js
@@ -8,23 +8,23 @@ import LogoWhiteFrameless from '../../Assets/Logo/LogoWhiteFrameless.png';
 
 const Menu = ({ menuList, isNavOpen, setIsNavOpen }) => {
   return (
-    <div className="py-5 px-8 bg-black">
-      <nav className="flex items-center justify-between pr-5">
+    <div className=" bg-black">
+      <nav className=" relative flex items-center justify-between pr-5">
         <div
           onClick={() => setIsNavOpen((open) => !open)}
           className={
             isNavOpen
-              ? 'hamburger-menu is-active fixed'
-              : 'hamburger-menu space-y-2 fixed'
+              ? 'hamburger-menu is-active fixed top-10 left-10'
+              : 'hamburger-menu space-y-2 fixed top-10 left-10'
           }
         >
           <div className="menu-icon"></div>
         </div>
-        <div className="">
+        <div className=" bg-blue-200">
           <div className={isNavOpen ? 'showMenuNav' : 'hideMenuNav'}>
             {' '}
             <div className="nav-menu flex justify-center flex-col gap-10 w-[80%] mx-[auto]">
-              <ul className="flex flex-col gap-10 md:gap-16 justify-between min-h-[250px] w-[80%] text-white text-6xl md:w-[80%] md:text-[6rem]">
+              <ul className="flex flex-col gap-6 md:gap-10 justify-between min-h-[250px] w-[100%] text-white text-6xl md:w-[80%] md:text-[6rem]">
                 {menuList.map((menu) => (
                   <MenuList
                     menu={menu}
@@ -36,9 +36,9 @@ const Menu = ({ menuList, isNavOpen, setIsNavOpen }) => {
                 ))}
               </ul>
               <div className="nav-menu-line h-[1px] bg-slate-400 "></div>
-              <div className="nav-menu-socials flex gap-4">
+              <div className="nav-menu-socials flex gap-6 md:gap-20">
                 <a
-                  className="transition hover:opacity-75 hover:line-through"
+                  className="transition hover:opacity-75 hover:border-b-2 hover:border-solid hover:border-b-white pb-2"
                   href="http://github.com/uisendy"
                   target="_blank"
                   rel="noreferrer"
@@ -46,7 +46,7 @@ const Menu = ({ menuList, isNavOpen, setIsNavOpen }) => {
                   <FaGithub className="text-2xl text-white" />
                 </a>
                 <a
-                  className="transition hover:opacity-75 hover:line-through"
+                  className="transition hover:opacity-75 hover:border-b-2 hover:border-solid hover:border-b-white pb-2"
                   href="https://www.linkedin.com/in/inieke-sendy-129b57ab/"
                   target="_blank"
                   rel="noreferrer"
@@ -54,7 +54,7 @@ const Menu = ({ menuList, isNavOpen, setIsNavOpen }) => {
                   <FaLinkedinIn className="text-2xl text-white" />
                 </a>
                 <a
-                  className="transition hover:opacity-75 hover:line-through"
+                  className="transition hover:opacity-75 hover:border-b-2 hover:border-solid hover:border-b-white pb-2"
                   href="https://twitter.com/SInieke/"
                   target="_blank"
                   rel="noreferrer"
@@ -65,15 +65,17 @@ const Menu = ({ menuList, isNavOpen, setIsNavOpen }) => {
             </div>
           </div>
         </div>
-        <div className="">
-          <Link className="text-white text-2xl md:text-4xl" to={'/'}>
-            <img
-              className="w-[50px] h-[50px]"
-              src={LogoWhiteFrameless}
-              alt="Logo"
-            />
-          </Link>
-        </div>
+        <Link
+          className="absolute top-10 right-12 z-50"
+          to={'/'}
+          onClick={() => setIsNavOpen(false)}
+        >
+          <img
+            className="w-[35px] h-[35px] md:w-[50px] md:h-[50px]"
+            src={LogoWhiteFrameless}
+            alt="Logo"
+          />
+        </Link>
       </nav>
     </div>
   );
