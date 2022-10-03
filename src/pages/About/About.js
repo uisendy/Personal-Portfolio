663a5b9b3126d495d213544edbd50e113e154763
Inieke Sendy <ini.e.sendy@gmail.com>
2022-10-03 19:01:05 +0100

    [f-about]: update: About page with Footer component

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -3,22 +3,27 @@ import Hero from './Hero';
 import Description from './Description';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
-import ScrollToTop from '../../components/ScrollDownIcon/ScrollToTop';
+import { useLocation } from 'react-router-dom';
 import Skills from './Skills';
 import Experience from './Experience';
+import Footer from '../../components/Footer/Footer';
 
 const About = () => {
   gsap.registerPlugin(ScrollTrigger);
+  const location = useLocation();
+
   useEffect(() => {
-    <ScrollToTop />;
     ScrollTrigger.refresh();
-  }, []);
+    window.scrollTo(0, 0);
+  }, [location]);
+
   return (
     <section className="bg-black text-white px-[6.5%]">
       <Hero />
       <Description />
       <Skills />
       <Experience />
+      <Footer />
     </section>
   );
 };
